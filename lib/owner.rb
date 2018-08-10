01dcbb4620f7813e39624e260cef82efcfa07731
Juan Tenezaca <tenj34@gmail.com>
2018-08-10 14:41:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+
+
+
+
+# => Class methods
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
